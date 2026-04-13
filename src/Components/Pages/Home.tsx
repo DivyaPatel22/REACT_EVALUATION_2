7c3eebc1b47b9b4827d2278b5014d13345ac2d36
DivyaPatel22 <dpatel.101122@gmail.com>
2026-04-13 16:44:50 +0530

    .

--- src/Components/Pages/Home.tsx
+++ src/Components/Pages/Home.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div>
         <h1>Welcome to Task-Manager</h1><br /><br />
-        <p>Defualt Credentials : </p><br /><br />
-        <p>name : 'abc'</p><br /><br />
+        <p>Defualt Credentials : </p>
+        <p>name : 'abc'</p>
         <p>Password : 1234</p>
     </div>
   )
